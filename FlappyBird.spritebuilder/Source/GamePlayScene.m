c4139f95e67ddfe46f7e05292eab1edc314012f0
wailyuen <stephen@mini-mac.local>
2014-10-23 22:08:28 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,7 +9,7 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
-}
+    [self addObstacle];}
 
 -(void)update:(CCTime)delta
 {
@@ -20,6 +20,5 @@
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
-    [self addObstacle];
-}
+   }
 @end
